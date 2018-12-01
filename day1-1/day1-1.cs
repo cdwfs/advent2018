2d508ec55444a12f511e91e9164d39e46d9de931
Cort <1944792+cdwfs@users.noreply.github.com>
2018-12-01 09:10:44 -0800

    Day 1 solutions

---
+++ day1-1/day1-1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace day1_1 {
+    class Program {
+        static void Main(string[] args) {
+            var inStream = new StreamReader(args[0]);
+            string line = "";
+            int freq = 0;
+            while ((line = inStream.ReadLine()) != null) {
+                freq += Int32.Parse(line);
+            }
+            Console.WriteLine($"freq = {freq}");
+        }
+    }
+}
---
+++ day1-2/day1-2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace day1_1 {
+    class Program {
+        static void Main(string[] args) {
+            int freq = 0;
+            var freqSet = new HashSet<int>();
+            while (true) {
+                var inStream = new StreamReader(args[0]);
+                string line = "";
+                while ((line = inStream.ReadLine()) != null) {
+                    freq += Int32.Parse(line);
+                    //Console.WriteLine($"freq = {freq}");
+                    if (!freqSet.Add(freq)) {
+                        Console.WriteLine($"first repeated freq = {freq}");
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
