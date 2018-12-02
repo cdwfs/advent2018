070516dbc9a45820df1f4278d2cdf3b2e861200e
Cort <1944792+cdwfs@users.noreply.github.com>
2018-12-02 08:45:05 -0800

    Day 2 solutions (brute force)

---
+++ day2-1/day2-1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day2_1 {
+    class Program {
+        static void Main(string[] args) {
+            var inStream = new StreamReader(args[0]);
+            string line = "";
+            int twoCount = 0;
+            int threeCount = 0;
+            while ((line = inStream.ReadLine()) != null) {
+                int[] letterCounts = new int[26]; // defaults to zero for all entries
+                foreach (char c in line) {
+                    int letterIndex = c - 'a';
+                    Debug.Assert(letterIndex >= 0 && letterIndex < 26, $"non-lowercase char {c} in string!");
+                    letterCounts[letterIndex] += 1;
+                }
+                bool hasTwo = false;
+                bool hasThree = false;
+                foreach (int count in letterCounts) {
+                    if (count == 2) {
+                        hasTwo = true;
+                    } else if (count == 3) {
+                        hasThree = true;
+                    }
+                }
+                if (hasTwo) {
+                    //Console.WriteLine($"{line} has 2x!");
+                    twoCount += 1;
+                } if (hasThree) {
+                    //Console.WriteLine($"{line} has 3x!");
+                    threeCount += 1;
+                }
+            }
+            Console.WriteLine($"twos: {twoCount}, threes: {threeCount}, product: {twoCount*threeCount}");
+        }
+    }
+}
---
+++ day2-2/day2-2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day2_2 {
+    class Program {
+        static void Main(string[] args) {
+            // Brute force ahoy!
+            var inStream = new StreamReader(args[0]);
+            string line = "";
+            List<string> lines = new List<string>();
+            while ((line = inStream.ReadLine()) != null) {
+                foreach (string other in lines) {
+                    Debug.Assert(other.Length == line.Length, "line length mismatch");
+                    int diffCount = 0;
+                    int diffIndex = -1;
+                    for (int i = 0; i < line.Length; ++i) {
+                        if (line[i] != other[i]) {
+                            diffCount += 1;
+                            diffIndex = i;
+                        }
+                    }
+                    if (diffCount == 1) {
+                        Console.WriteLine(line);
+                        Console.WriteLine(other);
+                        Console.WriteLine($"differ at index {diffIndex} ({line[diffIndex]} != {other[diffIndex]})");
+                        Console.WriteLine($"common chars: {line.Substring(0, diffIndex)}{line.Substring(diffIndex + 1)}");
+                        return;
+                    }
+                }
+                lines = lines.Append(line).ToList();
+            }
+            Console.WriteLine($"Found nothing?");
+        }
+    }
+}
