9b710f2c20d4a939bd566b8c07cef03d78b40db7
Cort Stratton <cort@unity3d.com>
2018-12-04 22:16:36 -0800

    day 5 solutions

---
+++ day05-1/day05-1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day05_1 {
+    class Program {
+        static void Main(string[] args) {
+            var inStream = new StreamReader(args[0]);
+            string input = inStream.ReadLine();
+            char[] output = new char[input.Length];
+            output[0] = input[0];
+            int outputSize = 1;
+            int caseDiff = Math.Abs('A' - 'a');
+            for (int i = 1; i < input.Length; ++i) {
+                if (outputSize > 0 && Math.Abs(output[outputSize-1] - input[i]) == caseDiff) {
+                    outputSize -= 1;
+                    continue;
+                }
+                output[outputSize] = input[i];
+                outputSize += 1;
+            }
+            string outStr = new string(output, 0, outputSize);
+            Console.WriteLine(outStr.Length);
+        }
+    }
+}
---
+++ day05-2/day05-2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day05_1 {
+    class Program {
+        static void Main(string[] args) {
+            var inStream = new StreamReader(args[0]);
+            string input = inStream.ReadLine();
+            int caseDiff = Math.Abs('A' - 'a');
+            for (char c = 'a'; c <= 'z'; ++c) {
+                char upper = Char.ToUpper(c);
+                char[] output = new char[input.Length];
+                int inputStart = 0;
+                do {
+                    output[0] = input[inputStart];
+                    inputStart += 1;
+                } while (output[0] == c || output[0] == upper);
+                int outputSize = 1;
+                for (int i = inputStart; i < input.Length; ++i) {
+                    if (input[i] == c || input[i] == upper) {
+                        continue;
+                    }
+                    if (outputSize > 0 && Math.Abs(output[outputSize - 1] - input[i]) == caseDiff) {
+                        outputSize -= 1;
+                        continue;
+                    }
+                    output[outputSize] = input[i];
+                    outputSize += 1;
+                }
+                string outStr = new string(output, 0, outputSize);
+                Console.WriteLine($"without {c}, length = {outStr.Length}");
+            }
+        }
+    }
+}
