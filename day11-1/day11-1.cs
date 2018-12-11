daff8f5dcf3c7094b537f03bd0f70e92b117534b
Cort Stratton <cort@unity3d.com>
2018-12-11 10:18:39 -0800

    day 11 solutions (no input required)

---
+++ day11-1/day11-1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace day11_1 {
+    class Program {
+        const Int64 SERIAL_NUMBER = 8444;
+        static Int64 CellPower(Int64 x, Int64 y) {
+            Int64 rackId = x + 10;
+            Int64 power = ((rackId * y) + SERIAL_NUMBER) * rackId;
+            return ((power / 100) % 10) - 5;
+        }
+        static void Main(string[] args) {
+            Int64[,] powers = new Int64[300,300];
+            Int64 maxPower = Int64.MinValue, maxX = -1, maxY = -1;
+            for (Int64 y = 299; y >= 0; --y) {
+                for (Int64 x = 299; x >= 0; --x) {
+                    powers[y, x] = CellPower(x, y);
+                    if (x + 3 < 300 && y + 3 < 300) {
+                        Int64 squarePower =
+                            powers[y + 0, x + 0] +
+                            powers[y + 0, x + 1] +
+                            powers[y + 0, x + 2] +
+                            powers[y + 1, x + 0] +
+                            powers[y + 1, x + 1] +
+                            powers[y + 1, x + 2] +
+                            powers[y + 2, x + 0] +
+                            powers[y + 2, x + 1] +
+                            powers[y + 2, x + 2];
+                        if (squarePower > maxPower) {
+                            maxPower = squarePower;
+                            maxX = x;
+                            maxY = y;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine($"Fuel cell at  {maxX},{maxY}, grid serial number {SERIAL_NUMBER}: power level {maxPower}");
+        }
+    }
+}
---
+++ day11-2/day11-2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace day11_2 {
+    class Program {
+        const Int64 SERIAL_NUMBER = 8444;
+        static Int64 CellPower(Int64 x, Int64 y) {
+            Int64 rackId = x + 10;
+            Int64 power = ((rackId * y) + SERIAL_NUMBER) * rackId;
+            return ((power / 100) % 10) - 5;
+        }
+        static void Main(string[] args) {
+            Int64[,] powers = new Int64[300, 300];
+            for (Int64 y = 0; y < 300; ++y) {
+                for (Int64 x = 0; x < 300; ++x) {
+                    powers[y, x] = CellPower(x, y);
+                }
+            }
+            Int64 maxPower = Int64.MinValue, maxX = -1, maxY = -1, maxS = -1;
+            Int64[,] sums = new long[300, 300];
+            for (Int64 s = 1; s <= 300; ++s) {
+                Console.WriteLine($"Testing {s}x{s} squares...");
+                for (Int64 y = 0; y < 300 - s + 1; ++y) {
+                    for (Int64 x = 0; x < 300 - s + 1; ++x) {
+                        Int64 squarePower = 0;
+                        if (s == 1) {
+                            squarePower = powers[y, x];
+                        } else {
+                            squarePower = sums[y, x];
+                            Int64 rowY = y + s - 1;
+                            Int64 colX = x + s - 1;
+                            for (Int64 sy = 0; sy < s - 1; ++sy) {
+                                squarePower += powers[y + sy, colX];
+                            }
+                            for (Int64 sx = 0; sx < s; ++sx) {
+                                squarePower += powers[rowY, x + sx];
+                            }
+                        }
+                        sums[y, x] = squarePower;
+                        if (squarePower > maxPower) {
+                            maxPower = squarePower;
+                            maxX = x;
+                            maxY = y;
+                            maxS = s;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine($"Square at  {maxX},{maxY},{maxS} with grid serial number {SERIAL_NUMBER}: power level {maxPower}");
+        }
+    }
+}
