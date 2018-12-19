7302af86fd73897712a82f945d22c2dd2cd5f894
Cort Stratton <cort@unity3d.com>
2018-12-19 13:34:33 -0800

    complete day 18 solutions

--- day18-2/day18-2.cs
+++ day18-2/day18-2.cs
@@ -6,9 +6,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace day18_1 {
+namespace day18_2 {
     class Program {
-        const int TICK_COUNT = 10;
+        const Int64 TICK_COUNT = 1000;
+        const Int64 TICK_TARGET = 1000000000;
         static int MAP_X_SIZE;
         static int MAP_Y_SIZE;
         static void PrintMap(char[,] map, string label) {
@@ -37,6 +38,27 @@ namespace day18_1 {
             }
             return count;
         }
+        static int MapHash(char[,] map) {
+            char[] flattened = new char[MAP_Y_SIZE * MAP_X_SIZE];
+            for (int y = 0; y < MAP_Y_SIZE; ++y) {
+                for (int x = 0; x < MAP_X_SIZE; ++x) {
+                    flattened[y * MAP_X_SIZE + x] = map[y, x];
+                }
+            }
+            return new string(flattened).GetHashCode();
+        }
+        static int MapValue(char[,] map) {
+            int forestCount = 0, lumberyardCount = 0;
+            for (int y = 0; y < MAP_Y_SIZE; ++y) {
+                for (int x = 0; x < MAP_X_SIZE; ++x) {
+                    if (map[y, x] == '|')
+                        forestCount += 1;
+                    else if (map[y, x] == '#')
+                        lumberyardCount += 1;
+                }
+            }
+            return forestCount * lumberyardCount;
+        }
         static string Solve(string inputFilename, bool enableLogs) {
             MAP_X_SIZE = 0;
             MAP_Y_SIZE = 0;
@@ -61,7 +83,7 @@ namespace day18_1 {
                 currentMap = mapA;
                 newMap = mapB;
                 int y = 0;
-                foreach(var row in lines) {
+                foreach (var row in lines) {
                     for (int x = 0; x < MAP_X_SIZE; ++x) {
                         currentMap[y, x] = row[x];
                     }
@@ -73,7 +95,12 @@ namespace day18_1 {
             }
 
             // Simulate
-            int elapsedTicks = 0;
+            Int64 elapsedTicks = 0;
+            var states = new Dictionary<int, (Int64 ticks, int value)>();
+            states[MapHash(currentMap)] = (0, MapValue(currentMap));
+            var tickValues = new Dictionary<Int64, int>();
+            tickValues[elapsedTicks] = MapValue(currentMap);
+            Int64 cycleLength = 0;
             while (elapsedTicks < TICK_COUNT) {
                 for (int y = 0; y < MAP_Y_SIZE; ++y) {
                     for (int x = 0; x < MAP_X_SIZE; ++x) {
@@ -82,7 +109,7 @@ namespace day18_1 {
                         } else if (currentMap[y, x] == '|') {
                             newMap[y, x] = CountAdjacent(x, y, '#', currentMap) >= 3 ? '#' : '|';
                         } else if (currentMap[y, x] == '#') {
-                            newMap[y, x] = (CountAdjacent(x, y, '#', currentMap) >= 1 && CountAdjacent(x,y,'|',currentMap) >= 1) ? '#' : '.';
+                            newMap[y, x] = (CountAdjacent(x, y, '#', currentMap) >= 1 && CountAdjacent(x, y, '|', currentMap) >= 1) ? '#' : '.';
                         }
                     }
                 }
@@ -91,21 +118,29 @@ namespace day18_1 {
                 if (enableLogs) {
                     PrintMap(currentMap, $"After {elapsedTicks} minutes:");
                 }
-            }
 
-            int forestCount = 0, lumberyardCount = 0;
-            for (int y = 0; y < MAP_Y_SIZE; ++y) {
-                for (int x = 0; x < MAP_X_SIZE; ++x) {
-                    if (currentMap[y, x] == '|')
-                        forestCount += 1;
-                    else if (currentMap[y, x] == '#')
-                        lumberyardCount += 1;
+                // TIL: hash of an array does not take into account array contents.
+                // TIL: char[].ToString() does not do what you think it does -- you get a name of the variable, not a string of its contents.
+                //      use new string(char[]) instead.
+                int value = MapValue(currentMap);
+                tickValues[elapsedTicks] = value;
+                int currentHash = MapHash(currentMap);
+                if (states.ContainsKey(currentHash)) {
+                    var oldState = states[currentHash];
+                    Debug.Assert(oldState.value == value);
+                    Console.WriteLine($"Cycle detected: turn {oldState.ticks} = turn {elapsedTicks} = value {oldState.value} (delta {elapsedTicks - oldState.ticks})");
+                    cycleLength = elapsedTicks - oldState.ticks;
+                    break;
+                } else {
+                    states[currentHash] = (elapsedTicks, value);
                 }
             }
-            return (forestCount*lumberyardCount).ToString();
+            Debug.Assert(cycleLength > 0, $"No cycle detected after {TICK_COUNT} ticks -- try more?");
+            Int64 cycleStart = elapsedTicks - cycleLength;
+            Int64 targetOffset = (TICK_TARGET - cycleStart) % cycleLength;
+            return tickValues[cycleStart + targetOffset].ToString();
         }
         static void Main(string[] args) {
-            Debug.Assert(Solve(args[0] + @"\day18-example0.txt", true) == "1147");
             Console.WriteLine(Solve(args[0] + @"\day18-input.txt", false));
         }
     }
